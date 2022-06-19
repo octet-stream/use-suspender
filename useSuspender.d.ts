ea7abb165374e8d6edf2c5e04b554fa9762ab814
Nick K <nick.kruchinin@gmail.com>
2022-06-19 17:13:21 +0300

    Small fix for code style in typings

--- useSuspender.d.ts
+++ useSuspender.d.ts
@@ -34,7 +34,8 @@ export interface SuspenderHook<TResult, TArgs extends unknown[]> {
  * @param ctx thisArg value
  */
 export function createSuspender<T extends SuspenderImplementation>(
-  fn: T, ctx?: unknown
+  fn: T,
+  ctx?: unknown
 ): SuspenderHook<UnwrapPromise<ReturnType<T>>, Parameters<T>>
 
 export default createSuspender
