cf62fed743e8be164bd6970b43f2a14eba429f4c
Nick K <nick.kruchinin@gmail.com>
2023-04-07 17:33:05 +0300

    Fixes for eslint and ava configuration

--- ava.config.js
+++ ava.config.js
@@ -4,6 +4,9 @@ export default {
     "--no-warnings",
     "--loader=ts-node/esm/transpile-only"
   ],
+  environmentVariables: {
+    "TS_NODE_PROJECT": "tsconfig.ava.json"
+  },
   extensions: {
     ts: "module",
     tsx: "module"
